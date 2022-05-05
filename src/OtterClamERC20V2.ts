a42a9f17a26ed2381026c7d630ef9466a7b8280f
abtheo <c.theo.kent@gmail.com>
2022-05-05 14:06:49 +0200

    Add Burn tracking on CLAM contract events

---
+++ src/OtterClamERC20V2.ts
@@ -0,0 +1,35 @@
+import { BigDecimal, ethereum } from '@graphprotocol/graph-ts'
+import { Transfer as TransferEvent } from '../generated/StakedOtterClamERC20V2/StakedOtterClamERC20V2'
+import { Transfer, TotalBurnedClam } from '../generated/schema'
+import { log } from '@graphprotocol/graph-ts'
+import { loadOrCreateTransaction } from './utils/Transactions'
+
+export function handleTransfer(event: TransferEvent): void {
+  if (event.params.from.toHexString() == '0x0000000000000000000000000000000000000000') {
+    let transaction = loadOrCreateTransaction(event.transaction, event.block)
+    let entity = new Transfer(transaction.id)
+    entity.from = event.params.from
+    entity.to = event.params.to
+    entity.value = event.params.value
+    entity.timestamp = transaction.timestamp
+    entity.transaction = transaction.id
+    entity.save()
+
+    let burnedClam = event.params.value.divDecimal(BigDecimal.fromString('1e9'))
+    log.debug('Burned CLAM {} at block {}', [burnedClam.toString(), event.block.number.toString()])
+
+    //Cumulative total for burned CLAM
+    let total = loadOrCreateTotalBurnedClamSingleton()
+    total.burnedClam = total.burnedClam.plus(burnedClam)
+    total.save()
+  }
+}
+
+export function loadOrCreateTotalBurnedClamSingleton(): TotalBurnedClam {
+  let total = TotalBurnedClam.load('1')
+  if (total == null) {
+    total = new TotalBurnedClam('1')
+    total.burnedClam = BigDecimal.fromString('0')
+  }
+  return total
+}
